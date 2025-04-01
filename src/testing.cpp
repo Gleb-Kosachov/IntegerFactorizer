de0aa03872149a5b93187ab22d3a77824455753d
Gleb-Kosachov <gkosachov99@gmail.com>
2025-04-01 19:47:12 +0300

    Improved the testing file

--- src/testing.cpp
+++ src/testing.cpp
@@ -23,117 +23,73 @@ int main()
         std::ofstream out;
         std::ifstream in;
         
-        out.open("src/Output/TrialDivision.txt");
         for (int i = 0; i < 7; i++)
         {
+            out.open("src/Output/TrialDivision.txt", std::ios::app);
             in.open("src/Semiprimes/" + sizes[i] + ".txt");
             if (!in.is_open()) throw std::runtime_error("File not found!");
             std::string Numbers[500];
             for (int i = 0; i < 500; i++) in >> Numbers[i];
             in.close();
-            uint64_t Time = 0;
-            bool LessThanTenMinutes = true;
+            std::chrono::high_resolution_clock::time_point begin = std::chrono::high_resolution_clock::now();
             for (int i = 0; i < 500; i++)
-            {
-                std::chrono::high_resolution_clock::time_point begin = std::chrono::high_resolution_clock::now();
                 BruteForce(Numbers[std::rand() % 500].data());
-                uint64_t TestTime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - begin).count();
-                if (TestTime > 600000)
-                {
-                    LessThanTenMinutes = false;
-                    break;
-                }
-                Time += TestTime;
-            }
-            if (!LessThanTenMinutes) break;
-            Time /= 500;
+            uint64_t Time = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now() - begin).count() / 500000000; 
             out << sizes[i] << ": " << Time << "\n";
+            out.close();
+	    if (Time >= 60000) break;
         }
-        out.close();
         
-        out.open("src/Output/Fermat.txt");
         for (int i = 0; i < 7; i++)
         {
+            out.open("src/Output/Fermat.txt", std::ios::app);
             in.open("src/Semiprimes/" + sizes[i] + ".txt");
             if (!in.is_open()) throw std::runtime_error("File not found!");
             std::string Numbers[500];
             for (int i = 0; i < 500; i++) in >> Numbers[i];
             in.close();
-            uint64_t Time = 0;
-            bool LessThanTenMinutes = true;
+            std::chrono::high_resolution_clock::time_point begin = std::chrono::high_resolution_clock::now();
             for (int i = 0; i < 500; i++)
-            {
-                std::chrono::high_resolution_clock::time_point begin = std::chrono::high_resolution_clock::now();
                 Fermat(Numbers[std::rand() % 500].data());
-                uint64_t TestTime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - begin).count();
-                if (TestTime > 600000)
-                {
-                    LessThanTenMinutes = false;
-                    break;
-                }
-                Time += TestTime;
-            }
-            if (!LessThanTenMinutes) break;
-            Time /= 500;
+            uint64_t Time = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now() - begin).count() / 500000000; 
             out << sizes[i] << ": " << Time << "\n";
+            out.close();
+	    if (Time >= 60000) break;
         }
-        out.close();
-        
-        out.open("src/Output/PollardRho.txt");
+
         for (int i = 0; i < 7; i++)
         {
+            out.open("src/Output/PollardRho.txt", std::ios::app);
             in.open("src/Semiprimes/" + sizes[i] + ".txt");
             if (!in.is_open()) throw std::runtime_error("File not found!");
             std::string Numbers[500];
             for (int i = 0; i < 500; i++) in >> Numbers[i];
             in.close();
-            uint64_t Time = 0;
-            bool LessThanTenMinutes = true;
+            std::chrono::high_resolution_clock::time_point begin = std::chrono::high_resolution_clock::now();
             for (int i = 0; i < 500; i++)
-            {
-                std::chrono::high_resolution_clock::time_point begin = std::chrono::high_resolution_clock::now();
                 PollardRho(Numbers[std::rand() % 500].data());
-                uint64_t TestTime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - begin).count();
-                if (TestTime > 600000)
-                {
-                    LessThanTenMinutes = false;
-                    break;
-                }
-                Time += TestTime;
-            }
-            if (!LessThanTenMinutes) break;
-            Time /= 500;
+            uint64_t Time = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now() - begin).count() / 500000000; 
             out << sizes[i] << ": " << Time << "\n";
+            out.close();
+	    if (Time >= 60000) break;
         }
-        out.close();
-        
-        out.open("src/Output/QuadraticSieve.txt");
+
         for (int i = 3; i < 7; i++)
         {
+            out.open("src/Output/QuadraticSieve.txt", std::ios::app);
             in.open("src/Semiprimes/" + sizes[i] + ".txt");
             if (!in.is_open()) throw std::runtime_error("File not found!");
             std::string Numbers[500];
             for (int i = 0; i < 500; i++) in >> Numbers[i];
             in.close();
-            uint64_t Time = 0;
-            bool LessThanTenMinutes = true;
+            std::chrono::high_resolution_clock::time_point begin = std::chrono::high_resolution_clock::now();
             for (int i = 0; i < 500; i++)
-            {
-                std::chrono::high_resolution_clock::time_point begin = std::chrono::high_resolution_clock::now();
                 QuadraticSieve(Numbers[std::rand() % 500].data());
-                uint64_t TestTime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - begin).count();
-                if (TestTime > 600000)
-                {
-                    LessThanTenMinutes = false;
-                    break;
-                }
-                Time += TestTime;
-            }
-            if (!LessThanTenMinutes) break;
-            Time /= 500;
+            uint64_t Time = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now() - begin).count() / 500000000; 
             out << sizes[i] << ": " << Time << "\n";
+            out.close();
+	    if (Time >= 60000) break;
         }
-        out.close();
     }
     catch (std::exception *a)
     {
